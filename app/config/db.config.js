db1135e31ac1b8dd0a854053cab57f34d036e01e
2020mt93666 <2020mt93666@wilp.bits-pilani.ac.in>
2022-04-03 12:54:28 +0530

    Made API asynchronous

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,6 +6,6 @@ const {
   DB_NAME,
 } = process.env;
 module.exports = {
-  // url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
-  url:'mongodb+srv://sezg583:123@cluster0.5nnxd.mongodb.net?retryWrites=true&w=majority'
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+  // url:'mongodb+srv://sezg583:123@cluster0.5nnxd.mongodb.net?retryWrites=true&w=majority'
 };
--- app/controllers/player.controller.js
+++ app/controllers/player.controller.js
@@ -2,10 +2,12 @@ const db = require("../models");
 const PlayerModel = db.players;
 
 // Create and Save a new player
-exports.create = (req, res) => {
+exports.create = async (req, res, next) => {
   // Validate request
   if (!req.body.title) {
-    res.status(400).send({ message: "Content can not be empty!" });
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
     return;
   }
 
@@ -16,56 +18,57 @@ exports.create = (req, res) => {
     published: req.body.published ? req.body.published : false
   });
 
-  // Save player in the database
-  player
-    .save(player)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the player."
-      });
+  try {
+    // Save player in the database
+    const data = await player.save();
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the player."
     });
+  }
 };
 
 // Retrieve all players from the database.
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   const title = req.query.title;
-  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  var condition = title ? {
+    title: {
+      $regex: new RegExp(title),
+      $options: "i"
+    }
+  } : {};
 
-  PlayerModel.find(condition)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving players."
-      });
+  try {
+    const data = await PlayerModel.find(condition);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving players."
     });
+  }
 };
 
 // Find a single player with an id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.id;
 
-  PlayerModel.findById(id)
-    .then(data => {
-      if (!data)
-        res.status(404).send({ message: "Not found player with id " + id });
-      else res.send(data);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ message: "Error retrieving player with id=" + id });
+  try {
+    const data = await PlayerModel.findById(id);
+    if (!data)
+      res.status(404).send({
+        message: "Not found player with id " + id
+      });
+    else res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: "Error retrieving player with id=" + id
     });
+  }
 };
 
 // Update a player by the id in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   if (!req.body) {
     return res.status(400).send({
       message: "Data to update can not be empty!"
@@ -74,40 +77,44 @@ exports.update = (req, res) => {
 
   const id = req.params.id;
 
-  PlayerModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot update player with id=${id}. Maybe player was not found!`
-        });
-      } else res.send({ message: "player was updated successfully." });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating player with id=" + id
-      });
+  try {
+    const data = await PlayerModel.findByIdAndUpdate(id, req.body, {
+      useFindAndModify: false
     });
+    if (!data) {
+      res.status(404).send({
+        message: `Cannot update player with id=${id}. Maybe player was not found!`
+      });
+    } else res.send({
+      message: "player was updated successfully."
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: "Error updating player with id=" + id
+    });
+  }
 };
 
 // Delete a player with the specified id in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
 
-  PlayerModel.findByIdAndRemove(id, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot delete player with id=${id}. Maybe player was not found!`
-        });
-      } else {
-        res.send({
-          message: "player was deleted successfully!"
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete player with id=" + id
-      });
+  try {
+    const data = await PlayerModel.findByIdAndRemove(id, {
+      useFindAndModify: false
     });
+    if (!data) {
+      res.status(404).send({
+        message: `Cannot delete player with id=${id}. Maybe player was not found!`
+      });
+    } else {
+      res.send({
+        message: "player was deleted successfully!"
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete player with id=" + id
+    });
+  }
 };
